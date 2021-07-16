57afa06074782177e0ab6d74d585d57b82bc10e3
cachong2 <cachong@atmiprecast.com>
2021-07-16 14:38:22 -0500

    Add project files.

---
+++ Design_Criteria/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Design_Criteria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_TeddsFile = new System.Windows.Forms.TextBox();
+            this.btn_OpenExplorer = new System.Windows.Forms.Button();
+            this.btn_PreviewDC = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_UpdateDC = new System.Windows.Forms.Button();
+            this.btn_OpenExistingTedds = new System.Windows.Forms.Button();
+            this.btn_GetVars = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Get Variables";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "OPEN EXISTING TEDDS FILE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "File Path";
+            // 
+            // txt_TeddsFile
+            // 
+            this.txt_TeddsFile.Location = new System.Drawing.Point(66, 41);
+            this.txt_TeddsFile.Name = "txt_TeddsFile";
+            this.txt_TeddsFile.Size = new System.Drawing.Size(171, 20);
+            this.txt_TeddsFile.TabIndex = 3;
+            this.txt_TeddsFile.TextChanged += new System.EventHandler(this.txt_TeddsFile_TextChanged);
+            // 
+            // btn_OpenExplorer
+            // 
+            this.btn_OpenExplorer.Location = new System.Drawing.Point(243, 39);
+            this.btn_OpenExplorer.Name = "btn_OpenExplorer";
+            this.btn_OpenExplorer.Size = new System.Drawing.Size(28, 23);
+            this.btn_OpenExplorer.TabIndex = 4;
+            this.btn_OpenExplorer.Text = "...";
+            this.btn_OpenExplorer.UseVisualStyleBackColor = true;
+            this.btn_OpenExplorer.Click += new System.EventHandler(this.btn_OpenExplorer_Click);
+            // 
+            // btn_PreviewDC
+            // 
+            this.btn_PreviewDC.Location = new System.Drawing.Point(102, 76);
+            this.btn_PreviewDC.Name = "btn_PreviewDC";
+            this.btn_PreviewDC.Size = new System.Drawing.Size(135, 23);
+            this.btn_PreviewDC.TabIndex = 7;
+            this.btn_PreviewDC.Text = "Preview Design Criteria";
+            this.btn_PreviewDC.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 120);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(902, 368);
+            this.listBox1.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "@\"C:\\\"";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.Title = "Browse Text Files";
+            // 
+            // btn_UpdateDC
+            // 
+            this.btn_UpdateDC.Location = new System.Drawing.Point(243, 76);
+            this.btn_UpdateDC.Name = "btn_UpdateDC";
+            this.btn_UpdateDC.Size = new System.Drawing.Size(137, 23);
+            this.btn_UpdateDC.TabIndex = 9;
+            this.btn_UpdateDC.Text = "Update Design Criteria";
+            this.btn_UpdateDC.UseVisualStyleBackColor = true;
+            this.btn_UpdateDC.Click += new System.EventHandler(this.btn_UpdateDC_Click);
+            // 
+            // btn_OpenExistingTedds
+            // 
+            this.btn_OpenExistingTedds.Location = new System.Drawing.Point(279, 39);
+            this.btn_OpenExistingTedds.Name = "btn_OpenExistingTedds";
+            this.btn_OpenExistingTedds.Size = new System.Drawing.Size(101, 23);
+            this.btn_OpenExistingTedds.TabIndex = 6;
+            this.btn_OpenExistingTedds.Text = "Open Tedds File";
+            this.btn_OpenExistingTedds.UseVisualStyleBackColor = true;
+            this.btn_OpenExistingTedds.Click += new System.EventHandler(this.btn_RunTedds_Click);
+            // 
+            // btn_GetVars
+            // 
+            this.btn_GetVars.Location = new System.Drawing.Point(15, 76);
+            this.btn_GetVars.Name = "btn_GetVars";
+            this.btn_GetVars.Size = new System.Drawing.Size(81, 23);
+            this.btn_GetVars.TabIndex = 10;
+            this.btn_GetVars.Text = "Get Variables";
+            this.btn_GetVars.UseVisualStyleBackColor = true;
+            this.btn_GetVars.Click += new System.EventHandler(this.btn_GetVars_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 513);
+            this.Controls.Add(this.btn_GetVars);
+            this.Controls.Add(this.btn_UpdateDC);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_PreviewDC);
+            this.Controls.Add(this.btn_OpenExistingTedds);
+            this.Controls.Add(this.btn_OpenExplorer);
+            this.Controls.Add(this.txt_TeddsFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_TeddsFile;
+        private System.Windows.Forms.Button btn_OpenExplorer;
+        private System.Windows.Forms.Button btn_PreviewDC;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_UpdateDC;
+        private System.Windows.Forms.Button btn_OpenExistingTedds;
+        private System.Windows.Forms.Button btn_GetVars;
+    }
+}
+
---
+++ Design_Criteria/Form1.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+using Tekla.Structures.Model;
+using TSMUI = Tekla.Structures.Model.UI;
+using Tekla.Structures.Geometry3d;
+using TSG = Tekla.Structures.Geometry3d;
+using TTC = Tekla.Structural.InteropAssemblies.TeddsCalc;
+using TT = Tekla.Structural.InteropAssemblies.Tedds;
+using ClosedXML.Excel;
+using MOIW = Microsoft.Office.Interop.Word;
+
+namespace Design_Criteria
+{
+
+    //TODO: VARIABLES FROM TEDDS FILE TO WORD - CREATE WORD FILE AFTER THE FACT - LOOK AT JASON'S CODE
+    //TODO: ASSIGN VARIABLES TO EXCEL FILE TO SHOW
+    //TODO: UPDATE INTERFACE
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string templatePath = "C:/Users/cachong/Desktop/Templates";
+
+            templatePathSheet = templatePath + "Design Criteria Template.xlsx";
+            existingFilePath = @"C:\Users\cachong\Desktop\";
+            fileName = "DESIGN CRITERIA - Copy.docx";
+            templatefile = templatePath + "Drawing List Template.xlsx";
+
+            string tempRead = "";
+            myModel.GetProjectInfo().GetUserProperty("FILE_PATH", ref tempRead);
+
+            txt_TeddsFile.Text = tempRead;
+
+            //reportPath = myModel.GetInfo().ModelPath + "/Reports/" + System.Text.RegularExpressions.Regex.Replace(myModel.GetInfo().ModelName, ".db1", "");
+            //existingFilePath = reportPath + "_DesignCriteria_(0).xlsx";
+        }
+
+       
+        #region Global Variables
+
+        IXLWorkbook myWorkbookXML;
+        IXLWorksheet mySheetXML;
+        Model myModel = new Model();
+        WorkPlaneHandler myWPHandler;
+        string templatePathSheet;
+        string existingFilePath;
+        string fileName;
+        string reportPath;
+        string filePath;
+        string templatefile;
+        public string docPath = "\\attributes";
+        string BuildingCode;
+        string Standard;
+        string RiskCategory;
+
+        #endregion
+
+        SortedDictionary<int, TeddsVariables> variableList = new SortedDictionary<int, TeddsVariables>();
+        SortedDictionary<int, TeddsVariables> DeadLoads = new SortedDictionary<int, TeddsVariables>();
+        SortedDictionary<int, TeddsVariables> LiveLoads = new SortedDictionary<int, TeddsVariables>();
+        List<string> variableNamesLL = new List<string>();
+
+        #region User Interface
+
+        private void btn_GetVars_Click(object sender, EventArgs e)
+        {
+            RunTedds(variableList, LiveLoads, DeadLoads);
+
+            listBox1.Items.Add("BUILDING CODE = " + BuildingCode);
+            listBox1.Items.Add("REFERENCED STANDARD = " + Standard);
+            listBox1.Items.Add("RISK CATEGORY = " + RiskCategory);
+            listBox1.Items.Add("");
+            listBox1.Items.Add("LIVE LOADS");
+            listBox1.Items.Add("");
+
+            foreach (var item in LiveLoads)
+            {
+                ListViewItem Item1 = new ListViewItem();
+                ListViewItem.ListViewSubItem subItem = new ListViewItem.ListViewSubItem();
+
+                if (item.Value.SubClass1Name == null && item.Value.SubClass2Name == null)
+                {
+                    Item1.Text = item.Value.ClassName + "  =  " + item.Value.Value;
+                    listBox1.Items.Add(Item1.Text);
+                }
+
+                else if (item.Value.SubClass1Name != null && item.Value.SubClass2Name == null)
+                {
+                    Item1.Text = item.Value.ClassName + "    |    " + item.Value.SubClass1Name + "  =  " + item.Value.Value;
+                    listBox1.Items.Add(Item1.Text);
+                }
+
+                else if (item.Value.SubClass1Name != null && item.Value.SubClass2Name != null)
+                {
+                    Item1.Text = item.Value.ClassName + "    |    " + item.Value.SubClass1Name + "    |    " + item.Value.SubClass2Name + "  =  " + item.Value.Value;
+                    listBox1.Items.Add(Item1.Text);
+                }
+
+
+            }
+
+            listBox1.Items.Add("");
+            listBox1.Items.Add("DEAD LOADS");
+            listBox1.Items.Add("");
+
+            foreach (var item in DeadLoads)
+            {
+                ListViewItem Item = new ListViewItem();
+                ListViewItem Item2 = new ListViewItem();
+
+                if (item.Value.FloorNumber.Equals("floor1"))
+                {
+                    Item.Text = item.Value.ClassName.ToString() + "   ||   " + item.Value.SubClass1Name.ToString() + "   ||   " + item.Value.SubClass2Name.ToString() + "   ||   " + item.Value.Value.ToString();
+                    Item2.Text = item.Value.FloorName;
+
+                    listBox1.Items.Add(Item.Text);
+                }
+
+                if (item.Value.FloorNumber.Equals("floor2"))
+                {
+                    Item.Text = item.Value.ClassName.ToString() + "   ||   " + item.Value.SubClass1Name.ToString() + "   ||   " + item.Value.SubClass2Name.ToString() + "   ||   " + item.Value.Value.ToString();
+                    Item2.Text = item.Value.FloorName;
+
+             
+                    listBox1.Items.Add(Item.Text);
+                }
+
+                if (item.Value.FloorNumber.Equals("floor3"))
+                {
+                    Item.Text = item.Value.ClassName.ToString() + "   ||   " + item.Value.SubClass1Name.ToString() + "   ||   " + item.Value.SubClass2Name.ToString() + "   ||   " + item.Value.Value.ToString();
+                    Item2.Text = item.Value.FloorName;
+
+                    listBox1.Items.Add(Item.Text);
+                }
+
+            }
+
+
+        }
+
+        private void txt_TeddsFile_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_OpenExplorer_Click(object sender, EventArgs e)
+        {
+            OpenExplorer();
+        }
+
+        private void btn_RunTedds_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void btn_UpdateDC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        void RunTedds(SortedDictionary<int, TeddsVariables> variableList, SortedDictionary<int, TeddsVariables> LiveLoads, SortedDictionary<int, TeddsVariables> DeadLoads)
+        {
+            string docPath = myModel.GetInfo().ModelPath + "\\attributes\\ATMI_" + "DesignCriteria" + "_TDS.txt";
+            string evalString = "EvalFile(\"" + docPath + "\", Append)";
+            int counter2 = 1;
+            char[] separator = new char[] { '\t' };
+
+            TTC.ICalculator calculator = new TTC.Calculator();
+            calculator.Initialize();
+
+            if (File.Exists(docPath))
+            {
+                calculator.Functions.Eval(evalString);
+            }
+
+            string variables = calculator.GetVariables();
+
+            calculator.Initialize("", variables);
+            calculator.Functions.Eval("EvalCalcItem(\"$(UserLbrDir)ATMI_PDDesignCriteria_v21.lbr\",\"PDDesignCriteria_v21\")");
+
+            BuildingCode = calculator.Functions.GetVar("BC").ToString();
+            Standard = calculator.Functions.GetVar("Standard").ToString();
+            RiskCategory = calculator.Functions.GetVar("Risk").ToString();
+
+            #region Get Load Names
+
+            //GetLiveLoads
+            for (int counter = 1; counter < 15; counter ++)
+            {
+                string value1 = "";
+                string value2 = "";
+                string value3 = "";
+
+                if (calculator.Functions.VarExists("rowSortedLoads" + counter.ToString()))
+                {
+                    TeddsVariables LL = new TeddsVariables();
+                    string variableName = calculator.Functions.GetVar("rowSortedLoads" + counter.ToString()).ToString();
+                    LL.ClassName = variableName.Split(separator)[0].ToString();
+
+                    value1 = variableName.Split(separator)[1].ToString();
+
+                    if (value1 != null)
+                    {
+                        LL.SubClass1Name = value1;
+                    }
+
+                    value2 = variableName.Split(separator)[2].ToString();
+
+                    if (value1 != null)
+                    {
+                        LL.SubClass2Name = value2;
+                    }
+
+                    LL.Value = variableName.Split(separator)[3].ToString();
+
+
+                    LiveLoads.Add(counter2, LL);
+                    counter2++;
+                }
+                
+            }
+            
+            //GetFirstFloorDeadLoads
+            for (double counter = 1; counter < 10; counter++)
+            {
+                if (calculator.Functions.VarExists("ClassD_1." + counter.ToString()) && calculator.Functions.VarExists("ValueD_1." + counter.ToString()))
+                {
+                   
+                    TeddsVariables floor1 = new TeddsVariables();
+                    floor1.FloorNumber = "floor1";
+                    floor1.ClassName = calculator.Functions.GetVar("ClassD_1." + counter.ToString()).ToString();
+                    floor1.SubClass1Name = calculator.Functions.GetVar("Sub1ClassD_1." + counter.ToString()).ToString();
+                    floor1.SubClass2Name = calculator.Functions.GetVar("Sub2ClassD_1." + counter.ToString()).ToString();
+                    floor1.Value = calculator.Functions.GetVar("ValueD_1." + counter.ToString()).ToString();
+                    floor1.FloorName = calculator.Functions.GetVar("Floor1").ToString();
+
+                    DeadLoads.Add(counter2, floor1);
+                    counter2++;
+                }
+            }
+
+            //GetSecondFloorDeadLoads
+            for (double counter = 1; counter < 10; counter++)
+            {
+                if (calculator.Functions.VarExists("ClassD_2." + counter.ToString()) && calculator.Functions.VarExists("ValueD_2." + counter.ToString()))
+                {
+
+                    TeddsVariables floor2 = new TeddsVariables();
+                    floor2.FloorNumber = "floor2";
+                    floor2.ClassName = calculator.Functions.GetVar("ClassD_2." + counter.ToString()).ToString();
+                    floor2.SubClass1Name = calculator.Functions.GetVar("Sub1ClassD_2." + counter.ToString()).ToString();
+                    floor2.SubClass2Name = calculator.Functions.GetVar("Sub2ClassD_2." + counter.ToString()).ToString();
+                    floor2.Value = calculator.Functions.GetVar("ValueD_2." + counter.ToString()).ToString();
+                    floor2.FloorName = calculator.Functions.GetVar("Floor2").ToString();
+
+                    DeadLoads.Add(counter2, floor2);
+                    counter2++;
+                }
+            }
+
+            //GetThirdFloorDeadLoads
+            for (double counter = 1; counter < 10; counter++)
+            {
+                if (calculator.Functions.VarExists("ClassD_3." + counter.ToString()) && calculator.Functions.VarExists("ValueD_1." + counter.ToString()))
+                {
+
+                    TeddsVariables floor3 = new TeddsVariables();
+                    floor3.FloorNumber = "floor3";
+                    floor3.ClassName = calculator.Functions.GetVar("ClassD_3." + counter.ToString()).ToString();
+                    floor3.SubClass1Name = calculator.Functions.GetVar("Sub1ClassD_3." + counter.ToString()).ToString();
+                    floor3.SubClass2Name = calculator.Functions.GetVar("Sub2ClassD_3." + counter.ToString()).ToString();
+                    floor3.Value = calculator.Functions.GetVar("ValueD_3." + counter.ToString()).ToString();
+                    floor3.FloorName = calculator.Functions.GetVar("Floor3").ToString();
+
+                    DeadLoads.Add(counter2, floor3);
+                    counter2++;
+                }
+            }
+
+            #endregion
+
+            evalString = "SaveSectionVarsTextFile(\"" + docPath + "\")";
+
+            calculator.Functions.Eval(evalString);
+
+            calculator = null;
+            GC.Collect();
+
+        }
+
+        #region Classes
+
+        public class TeddsVariables
+
+        {
+            public string FloorNumber = "";
+            public string FloorName = "";
+            public string LoadType = "";
+            public string ClassName = "";
+            public string SubClass1Name = "";
+            public string SubClass2Name = "";
+            public string Value = "";
+        }
+
+        #endregion
+
+        #region Excel
+        bool CreateNewWorksheet()
+        {
+            filePath = existingFilePath;
+            string worksheetName = "Design Criteria";
+
+            if (!File.Exists(filePath))
+            {
+                try
+                {
+                    File.Copy(templatefile, filePath);
+                    myWorkbookXML = new XLWorkbook(filePath);
+                    mySheetXML = myWorkbookXML.Worksheet(worksheetName);
+                    return true;
+                }
+
+                catch
+                {
+                    MessageBox.Show(this, "template report copying failed");
+                    return false;
+                }
+            }
+
+            else
+            {
+                myWorkbookXML = new XLWorkbook(filePath);
+                mySheetXML = myWorkbookXML.Worksheet(worksheetName);
+                return true;
+            }
+        }
+
+        bool EstablishExcel()
+        {
+            filePath = existingFilePath;
+            DirectoryInfo dInfo = new DirectoryInfo(myModel.GetInfo().ModelPath + "/Reports/");
+            FileInfo[] drawingLists = dInfo.GetFiles();
+
+            foreach (FileInfo file in drawingLists)
+            {
+                if (file.Name.Contains("_DesignCriteria_"))
+                {
+                    filePath = file.FullName;
+                }
+
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show(this, "Could not locate DrawingList");
+                }
+            }
+
+            myWorkbookXML = new XLWorkbook(filePath);
+            mySheetXML = myWorkbookXML.Worksheet("Design Criteria");
+            return true;
+        }
+
+        void CloseExcel()
+
+        {
+            myWorkbookXML.Save();
+            myWorkbookXML.Dispose();
+
+        }
+        void PopulateExcel()
+        {
+            if (EstablishExcel())
+            {
+
+            }
+
+        }
+
+        #endregion
+
+        #region Get Existing Tedds File
+        void OpenExplorer()
+
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                txt_TeddsFile.Text = filePath;
+            }
+
+        }
+        void OpenFile()
+        {
+            myModel.GetProjectInfo().SetUserProperty("FILE_PATH", filePath);
+            MOIW.Application ap = new MOIW.Application();
+            ap.Visible = true;
+            MOIW.Document document = ap.Documents.Open(filePath);
+        }
+
+
+        #endregion
+
+      
+    }
+}
